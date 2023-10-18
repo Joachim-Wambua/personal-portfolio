ea5a8d0fdf8886d9195bcfe36a0c46e74eb64f74
Joachim Wambua <kimwambua96@gmail.com>
2023-10-18 21:12:48 +0200

    front end changes to fonts, landing page bg & styling

---
+++ public/js/script.js
@@ -0,0 +1,164 @@
+  (function ($) {
+    "use strict";
+    $(window).on("load", function () {
+      $(".loader").fadeOut(1000);
+      var wow = new WOW({ offset: 150, mobile: false });
+      wow.init();
+    });
+    $(".animsition").animsition({
+      inClass: "fade-in",
+      outClass: "fade-out",
+      inDuration: 1000,
+      outDuration: 700,
+      linkElement: "a.project-box",
+      loading: true,
+      loadingParentElement: "body",
+      loadingClass: "spinner",
+      loadingInner:
+        '<div class="double-bounce1"></div><div class="double-bounce2"></div>',
+      timeout: false,
+      timeoutCountdown: 5000,
+      onLoadEvent: true,
+      browser: ["animation-duration", "-webkit-animation-duration"],
+      overlay: false,
+      overlayClass: "animsition-overlay-slide",
+      overlayParentElement: "body",
+      transition: function (url) {
+        window.location.href = url;
+      },
+    });
+    $(".popup-youtube").magnificPopup({
+      disableOn: 700,
+      type: "iframe",
+      mainClass: "mfp-with-zoom",
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false,
+    });
+    $(".navbar-toggle").on("click", function () {
+      $("body").removeClass("menu-is-closed").addClass("menu-is-opened");
+    });
+    $(".close-menu, .click-capture, .menu-list li a").on("click", function () {
+      $("body").removeClass("menu-is-opened").addClass("menu-is-closed");
+      $(".menu-list ul").slideUp(300);
+    });
+    $(".menu-list li a").on("click", function () {
+      $(".menu-list li").removeClass("active");
+      $(this).closest("li").addClass("active");
+    });
+    if ($(".owl-carousel").length > 0) {
+      $(".review-carousel").owlCarousel({
+        responsive: { 0: { items: 1 }, 720: { items: 1 }, 1280: { items: 1 } },
+        responsiveRefreshRate: 0,
+        nav: false,
+        navText: [],
+        animateIn: "fadeIn",
+        dots: true,
+      });
+    }
+    if ($(".pagepiling").length > 0) {
+      $(".pagepiling").pagepiling({
+        scrollingSpeed: 280,
+        loopBottom: true,
+        anchors: ["page1", "page2", "page3", "page4", "page5", "page6", "page7"],
+        afterLoad: function (anchorLink, index) {
+          if ($(".pp-section.active").scrollTop() > 0) {
+            $(".navbar").removeClass("navbar-white");
+          } else {
+            $(".navbar").addClass("navbar-white");
+          }
+        },
+      });
+    }
+    $("#pp-nav")
+      .remove()
+      .appendTo(".animsition")
+      .addClass("white right-boxed d-none d-sm-block");
+    $(".pp-nav-up").on("click", function () {
+      $.fn.pagepiling.moveSectionUp();
+    });
+    $(".pp-nav-down").on("click", function () {
+      $.fn.pagepiling.moveSectionDown();
+    });
+    $(".project-box").on("mouseover", function () {
+      var index = $(".project-box").index(this);
+      $(".bg-changer .section-bg")
+        .removeClass("active")
+        .eq(index)
+        .addClass("active");
+    });
+    if ($(".js-form").length) {
+      $(".js-form").each(function () {
+        $(this).validate({
+          errorClass: "error",
+          submitHandler: function (form) {
+            $.ajax({
+              type: "POST",
+              url: "mail.php",
+              data: $(form).serialize(),
+              success: function () {
+                $(".form-group-message").show();
+                $("#error").hide();
+                $("#success").show();
+              },
+              error: function () {
+                $(".form-group-message").show();
+                $("#success").hide();
+                $("#error").show();
+              },
+            });
+          },
+        });
+      });
+    }
+  })(jQuery);
+
+  // Get the current year
+  const currentYear = new Date().getFullYear();
+
+  // Find the element by its ID
+  const currentYearElement = document.getElementById("currentYear");
+
+  // Update the content of the element with the current year
+  currentYearElement.textContent = currentYear;
+
+  // Contact Submission Handler
+  function handleContactFormSubmit(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault;
+
+    // Get form input values
+    const name = document.getElementById("name").value;
+    const subject = document.getElementById("subject").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // Create a FormData object to send data as a multipart/form-data request
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("subject", subject);
+    formData.append("email", email);
+    formData.append("message", message);
+
+    // Send a POST request to your server (adjust the URL accordingly)
+    fetch("/submit-contact-message", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the server's response (e.g., show a success message)
+        console.log("Message submitted successfully:", data);
+        alert("Message submitted successfully.");
+      })
+      .catch((error) => {
+        // Handle errors (e.g., show an error message)
+        console.error("Error submitting Message:", error);
+        alert("An error occurred while submitting the Message.");
+      });
+  }
+
+  const contactForm = document.getElementById("contact-form");
+  if (contactForm) {
+    contactForm.addEventListener("submit", handleContactFormSubmit);
+  }
--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const bodyParser = require("body-parser");
 const dbModule = require("./config/db.js");
 const routes = require("./routes/index.js");
 const User = require("./dbSchemas/userSchema");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
 const bcrypt = require("bcrypt");
-const crypto = require("crypto"); // Import the crypto module
+const crypto = require("crypto"); // Import the crypto module;
+const flash = require("connect-flash");
 
 const port = process.env.PORT || 8080;
-
 const app = express();
 
 // Generate a secure secret key
@@ -21,75 +19,48 @@ const generateSecretKey = () => {
 
 const secretKey = generateSecretKey();
 
-const ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    // If the user is authenticated, allow access to the route
-    return next();
-  }
-  // If not authenticated, redirect to the login page or return an unauthorized response
-  res.redirect("/login"); // You can change this URL to your login page
-};
-
-
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Initialize express-session with the secret key
+app.use(flash());
+
+// Initialize express-session with custom options
 app.use(
   session({
     secret: secretKey,
     resave: false,
     saveUninitialized: false,
-    // You can customize other options as needed
+    // You can customize other options as needed for express-session
+    // For example:
+    // cookie: { secure: true }, // Use secure cookies for HTTPS
+    // name: "customSessionName", // Name of the session cookie
+    // store: customSessionStore, // Use a custom session store
+    // Other options as needed
   })
 );
 
-// Initialize Passport and configure session management
-app.use(passport.initialize());
-app.use(passport.session());
+// Custom middleware to check if the user is authenticated
+const ensureAuthenticated = (req, res, next) => {
+  if (req.session && req.session.user) {
+    return next(); // User is authenticated, proceed to the route
+  } else {
+    res.redirect("/login"); // User is not authenticated, redirect to the login page
+  }
+};
 
-// TODO: Authentication Middleware to protect admin routes
-
-// Passport Local Strategy for user authentication
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    // Replace this with your User model
-    User.findOne({ username }, async (err, user) => {
-      if (err) return done(err);
-      if (!user) return done(null, false, { message: "Incorrect username" });
-
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid)
-        return done(null, false, { message: "Incorrect password" });
-
-      return done(null, user);
-    });
-  })
-);
-
-// Serialize and deserialize user
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  // Replace this with your User model
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
 });
 
 app.use(express.static(path.join(__dirname, "public")));
 
-
 // Serve your HTML form page
-// Protect the "/admin" route with authentication
 app.get("/admin", ensureAuthenticated, (req, res) => {
   // This route is only accessible to authenticated users
   const filePath = path.join(__dirname, "public", "admin", "admin.html");
-  // console.log("File Path:", filePath);
   res.sendFile(filePath);
 });
 
@@ -97,37 +68,52 @@ app.get("/admin", ensureAuthenticated, (req, res) => {
 app.get("/login", (req, res) => {
   // Render your login page here
   const loginPagePath = path.join(__dirname, "public", "admin", "login.html");
-  // console.log("Login Page Path:", loginPagePath);
   res.sendFile(loginPagePath);
 });
 
 app.get("/registration", (req, res) => {
   // Render your login page here
   const registerPagePath = path.join(__dirname, "public", "admin", "registration.html");
-  // console.log("Login Page Path:", loginPagePath);
   res.sendFile(registerPagePath);
 });
 
 // Handle login form submission
-app.post("/login", passport.authenticate("local", {
-  successRedirect: "/admin", // Redirect to admin panel on successful login
-  failureRedirect: "/login", // Redirect to login page on failed login
-  failureFlash: true,
-}));
+app.post("/user-login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Replace this with your User model
+    const user = await User.findOne({ email });
+    if (!user) {
+      req.flash("error_msg", "Incorrect email");
+      res.redirect("/login");
+      return;
+    }
 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      req.flash("error_msg", "Incorrect password");
+      res.redirect("/login");
+      return;
+    }
 
-// Define your routes
-app.use("/", routes); // Using "/admin" as a base route for your admin-related routes
+    req.session.user = user; // Store the user in the session
+    req.flash("success_msg", "Login successful");
+    res.redirect("/admin");
+  } catch (err) {
+    console.error(err);
+    req.flash("error_msg", "An error occurred");
+    res.redirect("/login");
+  }
+});
 
-// Define a route for the root
+app.use("/", routes);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// Starting the server on the specified port
 app.listen(port, () => {
   console.log(`The application started successfully on port ${port}`);
 });
 
-// Connect to the database
 dbModule();
